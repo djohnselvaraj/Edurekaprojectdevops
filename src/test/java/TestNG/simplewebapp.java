1983275632463376d8c5064f7bc78dd7e5d1110e
Himanshu Shinde <jordan8438@gmail.com>
2020-05-29 10:17:00 +0530

    update 3

--- src/test/java/TestNG/simplewebapp.java
+++ src/test/java/TestNG/simplewebapp.java
@@ -13,7 +13,7 @@ public class simplewebapp {
 	WebDriver driver;
 	@BeforeMethod
 	public void launch() {
-		System.setProperty("webdriver.chrome.driver", "/opt/google/chromedriver/bin/chromedriver.sh");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
 		driver = new ChromeDriver(options);		
