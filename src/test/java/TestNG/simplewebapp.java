a7ae2268b35fdaf55e9e54ccf6ff27bbe0578616
Himanshu Shinde <jordan8438@gmail.com>
2020-05-30 15:03:10 +0530

    update 1

--- src/test/java/TestNG/simplewebapp.java
+++ src/test/java/TestNG/simplewebapp.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
 public class simplewebapp {
 	WebDriver driver;
 	@BeforeMethod
