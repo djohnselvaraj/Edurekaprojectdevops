5b6f5a09197b99724d37f1ef3cd95f278b195d03
himanshu shinde <Himanshu.shinde2u@gmail.com>
2020-06-05 17:59:40 +0530

    apple bite done

--- src/test/java/TestNG/simplewebapp.java
+++ src/test/java/TestNG/simplewebapp.java
@@ -17,7 +17,7 @@ public class simplewebapp {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
 		driver = new ChromeDriver(options);		
-		driver.get("http://ec2-3-17-14-99.us-east-2.compute.amazonaws.com:8001/index.php");
+		driver.get("http://ec2-18-188-147-222.us-east-2.compute.amazonaws.com:8001/index.php");
 		driver.manage().window().maximize();
 		
 	}
